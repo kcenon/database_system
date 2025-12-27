89885f4fd35efa124c385b1b414665ab3832f102
kcenon <kcenon@naver.com>
2025-12-27 11:51:57 +0900

    test(utils): add database_backend utilities and suppress deprecation warnings
    Update test utilities to support both legacy database_base and
    modern database_backend interfaces.
    
    - Add backend:: namespace with Result<T> helper functions
    - Add fixtures::users_data(), fixtures::products_data() for tests
    - Add assertions for Result<T> and VoidResult types
    - Add scoped_test_backend RAII wrapper
    - Suppress deprecation warnings in legacy mock headers
    
    Part of #289

--- tests/mocks/mock_database.h
+++ tests/mocks/mock_database.h
@@ -7,6 +7,18 @@ All rights reserved.
 
 #pragma once
 
+// Suppress deprecation warnings for legacy interface support
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
+
 #include "database/database_base.h"
 #include "mock_expectations.h"
 #include <queue>
@@ -136,3 +148,12 @@ private:
 };
 
 } // namespace database::testing
+
+// Restore diagnostic settings
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#elif defined(_MSC_VER)
+#pragma warning(pop)
+#endif

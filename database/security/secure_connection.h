e075e255d90bac9a02bf8a840e43c4325ec335ae
kcenon <kcenon@naver.com>
2026-03-20 13:25:36 +0900

    fix(security): add missing include for core::database_value
    The secure_connection.h header references core::database_value in the
    query_security::convert_to_prepared_statement() declaration but does not
    include database/core/database_backend.h where the type is defined.
    
    This was exposed when secure_connection.cpp was added to the build,
    triggering compilation of the header for the first time.

--- database/security/secure_connection.h
+++ database/security/secure_connection.h
@@ -33,6 +33,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 #include "../database_types.h"
+#include "../core/database_backend.h"
 #include <string>
 #include <vector>
 #include <memory>
